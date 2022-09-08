84471c67af3c13964873c21ede3e02672abc5408
vica <vica@mail.com>
2022-09-08 14:02:01 +0400

    Ex4: GET-запрос

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,11 +1,17 @@
 package test.java;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
     @Test
     public void testHelloWorld() {
-        System.out.println("Hello world Victoria");
+        Response response = RestAssured
+                .get ("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.body().print();
+
     }
 }
 
