95a019f07269200af9ed39718da1fc52035b1bca
Shelkovica <senvica@mail.ru>
2022-12-27 09:26:27 +0400

    Тесты по лекции

--- src/test/java/tests/UserGetTest.java
+++ src/test/java/tests/UserGetTest.java
@@ -1,9 +1,12 @@
 package tests;
 
+import io.qameta.allure.Description;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import lib.ApiCoreRequests;
 import lib.Assertions;
 import lib.BaseTestCase;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -11,11 +14,15 @@ import java.util.Map;
 
 public class UserGetTest extends BaseTestCase {
 
+    private  final ApiCoreRequests apiCoreRequests = new ApiCoreRequests();
+    String cookie;
+    String header;
+
     @Test
+    @Description("This test get user without cookie and token")
+    @DisplayName("Test get user with not authorization")
     public void testGetUserDataNotAuth(){
-        Response responseUserData = RestAssured
-                .get("https://playground.learnqa.ru/api/user/2")
-                .andReturn();
+        Response responseUserData = apiCoreRequests.makeGetRequestWithoutCookieAndToken("https://playground.learnqa.ru/api/user/2");
 
         Assertions.assertJsonHasField(responseUserData, "username");
         Assertions.assertJsonHasNotField(responseUserData, "firstName");
@@ -24,29 +31,47 @@ public class UserGetTest extends BaseTestCase {
     }
 
     @Test
+    @Description("This test get user after auth")
+    @DisplayName("Test get user after authorization")
     public void testGetUserDetailsAuthAsSameUser(){
         Map<String, String> authData = new HashMap<>();
         authData.put("email", "vinkotov@example.com");
         authData.put("password", "1234");
 
-        Response responseGetAuth = RestAssured
-                .given()
-                .body(authData)
-                .post("https://playground.learnqa.ru/api/user/login")
-                .andReturn();
+        Response responseGetAuth = apiCoreRequests
+                .makePostRequest("https://playground.learnqa.ru/api/user/login", authData);
 
         String header = this.getHeader(responseGetAuth, "x-csrf-token");
         String cookie = this.getCookie(responseGetAuth, "auth_sid");
 
-        Response responseUserData = RestAssured
-                .given()
-                .header("x-csrf-token", header)
-                .cookie("auth_sid", cookie)
-                .get("https://playground.learnqa.ru/api/user/2")
-                .andReturn();
+        Response responseUserData = apiCoreRequests.makeGetRequest("https://playground.learnqa.ru/api/user/2",
+                header,
+                cookie);
 
         String[] expectedFields = {"username","firstName", "lastName", "email" };
         Assertions.assertJsonHasFields(responseUserData, expectedFields);
+    }
+//ex 16
+    @Test
+    @Description("This test get another user after auth")
+    @DisplayName("Test get another user after authorization")
+    public void testGetUserAuthAsAnotherUser(){
+        Map<String, String> authData = new HashMap<>();
+        authData.put("email", "vinkotov@example.com");
+        authData.put("password", "1234");
+        Response responseGetAuth = apiCoreRequests
+                .makePostRequest("https://playground.learnqa.ru/api/user/login", authData);
 
+        String header = this.getHeader(responseGetAuth, "x-csrf-token");
+        String cookie = this.getCookie(responseGetAuth, "auth_sid");
+
+        Response responseUserData = apiCoreRequests.makeGetRequest("https://playground.learnqa.ru/api/user/1",
+                header,
+                cookie);
+
+        Assertions.assertJsonHasField(responseUserData, "username");
+        Assertions.assertJsonHasNotField(responseUserData, "firstName");
+        Assertions.assertJsonHasNotField(responseUserData, "lastName");
+        Assertions.assertJsonHasNotField(responseUserData, "email");
     }
 }
