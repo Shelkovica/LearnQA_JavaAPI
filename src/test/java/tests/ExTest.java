66bfc8a48dca957c093904bc4537264f0591b988
Shelkovica <senvica@mail.ru>
2022-12-29 10:44:23 +0400

    Ex9*: Подбор пароля

--- src/test/java/tests/ExTest.java
+++ src/test/java/tests/ExTest.java
@@ -102,6 +102,32 @@ public class ExTest extends BaseTestCase {
         Assertions.assertJsonHasField(responseAfterTask, "result");
     }
 
+
+    @ParameterizedTest
+    @ValueSource(strings = {"123456","123456789","qwerty","password","1234567","12345678","12345","iloveyou","111111","123123","abc123","qwerty123","1q2w3e4r","admin","qwertyuiop","654321","555555","lovely","7777777","welcome","888888","princess","dragon","password1","123qwe", })
+    public void testEx9(String pass) {
+        Map<String, String> query = new HashMap<>();
+        query.put("login","super_admin");
+        query.put("password",pass);
+        Response response = RestAssured
+                .given()
+                .params(query)
+                .post ("https://playground.learnqa.ru/ajax/api/get_secret_password_homework")
+                .andReturn();
+        response.prettyPrint();
+        String auth_cookie = response.getCookie("auth_cookie");
+
+        Response responseCheck = RestAssured
+                .given()
+                .cookie("auth_cookie",auth_cookie)
+                .get ("https://playground.learnqa.ru/ajax/api/check_auth_cookie")
+                .andReturn();
+
+        //responseCheck.prettyPrint();
+        Assertions.assertResponseTextEquals(responseCheck,"You are authorized");
+    }
+
+
     @Test
     public void testEx10() {
     String checkString = "Какая-то строка!";
